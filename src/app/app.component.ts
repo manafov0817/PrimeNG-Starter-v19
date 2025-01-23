18932079d04201248e13da215b5439128ef502a8
Mahammad Manafov <manafov0817@gmail.com>
2025-01-24 00:36:52 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,108 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { MessageModule } from 'primeng/message';
+import { CommonModule } from '@angular/common';
+import { MenuItem } from 'primeng/api';
+import { PanelMenu } from 'primeng/panelmenu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    InputTextModule,
+    ButtonModule,
+    MessageModule,
+    FormsModule,
+    PanelMenu,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'gainzlog-admin';
+export class AppComponent implements OnInit {
+  items!: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Files',
+        icon: 'pi pi-file',
+        items: [
+          {
+            label: 'Documents',
+            icon: 'pi pi-file',
+            items: [
+              {
+                label: 'Invoices',
+                icon: 'pi pi-file-pdf',
+                items: [
+                  {
+                    label: 'Pending',
+                    icon: 'pi pi-stop',
+                  },
+                  {
+                    label: 'Paid',
+                    icon: 'pi pi-check-circle',
+                  },
+                ],
+              },
+              {
+                label: 'Clients',
+                icon: 'pi pi-users',
+              },
+            ],
+          },
+          {
+            label: 'Images',
+            icon: 'pi pi-image',
+            items: [
+              {
+                label: 'Logos',
+                icon: 'pi pi-image',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Cloud',
+        icon: 'pi pi-cloud',
+        items: [
+          {
+            label: 'Upload',
+            icon: 'pi pi-cloud-upload',
+          },
+          {
+            label: 'Download',
+            icon: 'pi pi-cloud-download',
+          },
+          {
+            label: 'Sync',
+            icon: 'pi pi-refresh',
+          },
+        ],
+      },
+      {
+        label: 'Devices',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Phone',
+            icon: 'pi pi-mobile',
+          },
+          {
+            label: 'Desktop',
+            icon: 'pi pi-desktop',
+          },
+          {
+            label: 'Tablet',
+            icon: 'pi pi-tablet',
+          },
+        ],
+      },
+    ];
+  }
 }
